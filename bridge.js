6e654922ca9233cea649469be6a8a07564f3e407
Dan Bogert <dan.bogert@gmail.com>
2016-08-26 22:27:45 -0400

    finished adding scoring

--- bridge.js
+++ bridge.js
@@ -58,6 +58,14 @@ var Vulnerable = {
   ALL: 4,
 };
 
+var Suit = {
+  DIAMOND: 1,
+  CLUB: 2,
+  HEART: 3,
+  SPADE: 4,
+  NO_TRUMP: 5,
+}
+
 var thisEvent;
 
 $(function() {
@@ -156,8 +164,7 @@ function createEvent() {
   });
 
   $('.selectpicker').selectpicker({
-    style: 'btn-default',
-    size: 4
+    style: 'btn-default'
   });
 
 }
@@ -240,8 +247,8 @@ function createScoringAccordion(bridgeEvent, ns_pairs, ew_pairs) {
             "<div class='col-sm-2'>Contract</div>" +
             "<div class='col-sm-2'>By</div>" +
             "<div class='col-sm-2'>Tricks</div>" +
-            "<div class='col-sm-1'>Score</div>" +
-            "<div class='col-sm-1'>MPs</div>" +
+            "<div class='col-sm-1'>N/S Score</div>" +
+            "<div class='col-sm-1'>E/W Score</div>" +
           "</div>" +
           createScoringForms(hands[hand_num - 1], ns_pairs, ew_pairs, bridgeEvent) +
         "</div>" +
@@ -266,14 +273,14 @@ function createScoringForms(hand, ns_pairs, ew_pairs, bridgeEvent) {
   for (var i = 0; i < table_hands.length; i++) {
     content += "<div class='row'>" +
                   "<form id='" + hand.number + "-" + i + "-form' action='javascript:void(0);'>" +
-                    "<div class='col-sm-1'><input type='text' id='" + hand.number + "-" + i + "-board' value='" + table_hands[i].board.number + "' disabled></div>" +
-                    "<div class='col-sm-1'><input type='text' id='" + hand.number + "-" + i + "-ns' value='" + table_hands[i].ns_pair.number + "' disabled></div>" +
+                    "<div class='col-sm-1'><input class='text-only' type='text' id='" + hand.number + "-" + i + "-board' value='" + table_hands[i].board.number + "'></div>" +
+                    "<div class='col-sm-1'><input class='text-only' type='text' id='" + hand.number + "-" + i + "-ns' value='" + table_hands[i].ns_pair.number + "' disabled></div>" +
                     "<div class='col-sm-2'>" + createDropdown(hand.number + '-' + i, "-ew", "E/W", ew_pair_numbers, bridgeEvent) + "</div>" +
-                    "<div class='col-sm-2'><input type='text' pattern='^[1-7][SsDdCcHhNn]([Xx])?([Xx])?$' class='form-control uppercase' id='" + hand.number + "-" + i + "-contract' required></div>" +
+                    "<div class='col-sm-2'><input type='text' pattern='^[1-7][SsDdCcHhNn]([Xx])?([Xx])?$' class='form-control uppercase' id='" + hand.number + "-" + i + "-contract'  oninput='isRowComplete(\"" + hand.number + "-" + i + "\")' required></div>" +
                     "<div class='col-sm-2'>" + createDropdown(hand.number + '-' + i, "-by", "By", ["North", "South", "East", "West"], bridgeEvent) + "</div>" +
                     "<div class='col-sm-2'>" + createDropdown(hand.number + '-' + i, "-tricks", "Contract", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], bridgeEvent) + "</div>" +
-                    "<div class='col-sm-1'>50</div>" +
-                    "<div class='col-sm-1'>10</div>" +
+                    "<div class='col-sm-1'><input class='text-only' type='text' id='" + hand.number + "-" + i + "-nsscore'></div>" +
+                    "<div class='col-sm-1'><input class='text-only' type='text' id='" + hand.number + "-" + i + "-ewscore'></div>" +
                     "<button id='" + hand.number + "-" + i + "-submit' type='submit' class='btn btn-primary hidden'>Submit</button>" +
                   "</form>" +
                 "</div>";
@@ -304,18 +311,18 @@ function isValid(table_hand_id) {
   return $("#" + table_hand_id)[0].checkValidity();
 }
 
-function calculateScore(table_hand_id) {
-  var board_number = $("#" + table_hand_id + "-board").val();
-  var ns_number = $("#" + table_hand_id + "-ns").val();
-  var ew_number = $("#" + table_hand_id + "-ew").val();
-  var contract = $("#" + table_hand_id + "-contract").val().toUpperCase();
-  var declarer = $("#" + table_hand_id + "-by").val();
-  var taken_num_tricks = $("#" + table_hand_id + "-tricks").val();
+function calculateScore(full_table_hand_id) {
+  var board_number = $("#" + full_table_hand_id + "-board").val();
+  var ns_number = $("#" + full_table_hand_id + "-ns").val();
+  var ew_number = $("#" + full_table_hand_id + "-ew").val();
+  var contract = $("#" + full_table_hand_id + "-contract").val().toUpperCase();
+  var declarer = $("#" + full_table_hand_id + "-by").val();
+  var taken_num_tricks = $("#" + full_table_hand_id + "-tricks").val();
 
   var doubled = contract.indexOf("X") > 0;
   var redoubled = contract.indexOf("XX") > 0;
 
-  var ids = table_hand_id.split("-");
+  var ids = full_table_hand_id.split("-");
   var hand_id = parseInt(ids[0]) - 1;
   var table_hand_id = parseInt(ids[1]);
   var board = thisEvent.hands[hand_id].table_hands[table_hand_id].board;
@@ -327,23 +334,67 @@ function calculateScore(table_hand_id) {
     var vulnerable = isDeclarerVulnerable(declarer, board);
     var penalty_points = calculatePenaltyPoints(difference_num_tricks * -1, vulnerable, doubled, redoubled);
 
-    console.log("penalty: " + penalty_points);
+    if (declarer === "North" || declarer === "South") {
+      $("#" + full_table_hand_id + "-nsscore").val(penalty_points * -1);
+      $("#" + full_table_hand_id + "-ewscore").val(penalty_points);
+    } else {
+      $("#" + full_table_hand_id + "-nsscore").val(penalty_points);
+      $("#" + full_table_hand_id + "-ewscore").val(penalty_points * -1);
+    }
   } else {
-    // calculate points!!
+    var vulnerable = isDefenderVulnerable(declarer, board);
+    var suit = getSuit(contract);
+    var victory_points = calculateVictoryPoints(contract_num_tricks, difference_num_tricks, suit, vulnerable, doubled, redoubled);
+
+    if (declarer === "North" || declarer === "South") {
+      $("#" + full_table_hand_id + "-nsscore").val(victory_points);
+      $("#" + full_table_hand_id + "-ewscore").val(victory_points * -1);
+    } else {
+      $("#" + full_table_hand_id + "-nsscore").val(victory_points * -1);
+      $("#" + full_table_hand_id + "-ewscore").val(victory_points);
+    }
   }
 }
 
-function isDeclarerVulnerable(player, board) {
+function isDeclarerVulnerable(declarer, board) {
   if (board.vulnerable === Vulnerable.ALL) {
     return true;
   } else if (board.vulnerable === Vulnerable.NONE) {
     return false;
-  } else if (board.vulnerable === Vulnerable.NS && (player === "North" || player === "South")) {
+  } else if (board.vulnerable === Vulnerable.NS && (declarer === "North" || declarer === "South")) {
     return true;
-  } else if (board.vulnerable === Vulnerable.EW && (player === "East" || player === "West")) {
+  } else if (board.vulnerable === Vulnerable.EW && (declarer === "East" || declarer === "West")) {
     return true;
   } else {
     return false;
+  }
+}
+
+function isDefenderVulnerable(declarer, board) {
+  if (board.vulnerable === Vulnerable.ALL) {
+    return true;
+  } else if (board.vulnerable === Vulnerable.NONE) {
+    return false;
+  } else if (board.vulnerable === Vulnerable.NS && (declarer === "East" || declarer === "West")) {
+    return true;
+  } else if (board.vulnerable === Vulnerable.EW && (declarer === "North" || declarer === "South")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getSuit(contract) {
+  if (contract.indexOf("D") > 0) {
+    return Suit.DIAMOND;
+  } else if (contract.indexOf("C") > 0) {
+    return Suit.CLUB;
+  } else if (contract.indexOf("H") > 0) {
+    return Suit.HEART;
+  } else if (contract.indexOf("S") > 0) {
+    return Suit.SPADE;
+  } else {
+    return Suit.NO_TRUMP;
   }
 }
 
@@ -405,4 +456,119 @@ function getUndoubledPenalty(vulnerable) {
   } else {
     return 50;
   }
+}
+
+function calculateVictoryPoints(contract_num_tricks, num_overtricks, suit, vulnerable, doubled, redoubled) {
+  var victory_points = 0;
+
+  var contract_points = getContractPoints(contract_num_tricks, suit, doubled, redoubled);
+  victory_points += contract_points;
+  victory_points += getOvertrickPoints(num_overtricks, suit, vulnerable, doubled, redoubled);
+  victory_points += getSlamBonus(contract_num_tricks, vulnerable);
+  victory_points += getDoubledRedoubledBonus(doubled, redoubled);
+  victory_points += getGameBonus(contract_points, vulnerable);
+
+  return victory_points;
+}
+
+function getContractPoints(contract_num_tricks, suit, doubled, redoubled) {
+  var contract_points;
+
+  if (suit === Suit.NO_TRUMP) {
+    contract_points = 40;
+    contract_points += 30 * (contract_num_tricks - 1);
+  } else if (suit === Suit.SPADE || suit === Suit.HEART) {
+    contract_points = 30 * contract_num_tricks;
+  } else {
+    contract_points = 20 * contract_num_tricks;
+  }
+
+  if (doubled) {
+    contract_points *= 2;
+  }
+
+  if (redoubled) {
+    contract_points *= 2;
+  }
+
+  console.log("contract points: " + contract_points);
+  return contract_points;
+}
+
+function getOvertrickPoints(num_overtricks, suit, vulnerable, doubled, redoubled) {
+  var overtrick_points;
+
+  if (redoubled) {
+    if (vulnerable) {
+      overtrick_points = 400 * num_overtricks;
+    } else {
+      overtrick_points = 200 * num_overtricks;
+    }
+  } else if (doubled) {
+    if (vulnerable) {
+      overtrick_points = 200 * num_overtricks;
+    } else {
+      overtrick_points = 100 * num_overtricks;
+    }
+  } else {
+    if (suit === Suit.NO_TRUMP || suit === Suit.SPADE || suit === Suit.HEART) {
+      overtrick_points = 30 * num_overtricks;
+    } else {
+      overtrick_points = 20 * num_overtricks;
+    }
+  }
+
+  console.log("overtrick points: " + overtrick_points);
+  return overtrick_points;
+}
+
+function getSlamBonus(contract_num_tricks, vulnerable) {
+  var slam_bonus = 0;
+
+  if (contract_num_tricks + 6 === 12) {
+    if (vulnerable) {
+      slam_bonus = 750;
+    } else {
+      slam_bonus = 500;
+    }
+  } else if (contract_num_tricks + 6 === 13) {
+    if (vulnerable) {
+      slam_bonus = 1500;
+    } else {
+      slam_bonus = 1000;
+    }
+  }
+
+  console.log("slam bonus: " + slam_bonus);
+  return slam_bonus;
+}
+
+function getDoubledRedoubledBonus(doubled, redoubled) {
+  var doubled_redoubled_bonus = 0;
+
+  if (redoubled) {
+    doubled_redoubled_bonus = 100;
+  } else if (doubled) {
+    doubled_redoubled_bonus = 50;
+  }
+
+  console.log("doubled/redoubled bonus: " + doubled_redoubled_bonus);
+  return doubled_redoubled_bonus;
+}
+
+function getGameBonus(contract_points, vulnerable) {
+  var game_bonus;
+
+  if (contract_points < 100) {
+    game_bonus = 50;
+  } else {
+    if (vulnerable) {
+      game_bonus = 500;
+    } else {
+      game_bonus = 300;
+    }
+  }
+
+  console.log("game bonus: " + game_bonus);
+  return game_bonus;
 }
