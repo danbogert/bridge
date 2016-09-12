fe0e0429cdf2c786c2e092d05e9539ea4ceaf119
Dan Bogert <dan.bogert@gmail.com>
2016-09-11 20:00:30 -0400

    adding mongo dependencies

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var mongodb = require('mongodb');
+
 var app = express();
+
+// process.env.MONGODB_URI
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
